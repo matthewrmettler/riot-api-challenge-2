962032b8c6af1c6582259b6a92915c375a0f40f3
Matthew Mettler <matthewrmettler@gmail.com>
2015-08-31 18:44:00 -0400

    Done for now with css and web stuff.
    TODO: Most popular picks pie chart.

--- static/js/jquery_helper.js
+++ static/js/jquery_helper.js
@@ -1,3 +1,19 @@
 /**
  * Created by Matt on 8/31/2015.
  */
+
+// Smooth scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
